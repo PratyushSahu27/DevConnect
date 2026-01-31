56502eed2a2610019cf79194e03db322b253dc2d
Pratyush Sahu <sahupratyush27@gmail.com>
2026-02-01 00:59:33 +0530

    Initial commit for gateway

---
+++ backend/gateway/src/test/java/com/DevConnect/Gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevConnect.Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
