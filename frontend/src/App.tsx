bc76348a6cc7812bcd11e247b9b2cb89a46ee931
Pratyush Sahu <sahupratyush27@gmail.com>
2026-02-03 01:23:46 +0530

    fix: fixed code editor width

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import CollaborativeEditor from './components/CollaborativeEditor'
 function App() {
 
   return (
-    <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
+    <div style={{ height: '100vh', width: '90vw', display: 'flex', flexDirection: 'column' }}>
       <header style={{ color: 'white', borderBottom: '1px solid #f3f3f3ff' }}>
         <h2>DevConnect</h2>
       </header>
